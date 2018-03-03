9a73d6deb7823c31688135c9aeeb3fbebae1dd8a
T.Hrytsaienko <kortatiana89@gmail.com>
2018-03-03 15:39:16 +0200

    Keys for tr while maping added

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,18 +5,18 @@ const Books = (props) => (
     <tbody>
         {
             props.books.map((book, index) => {
-                <tr key={index}>
-                    <td>{book.title}</td>
-                    <td>{book.author}</td>
-                    <td>{book.isbn}</td>
-                    <td><a href="#" className="delete">X</a></td>
-                </tr>
-                // <BookItem 
-                //     key={index}
-                //     bookTitle={book.title}
-                //     bookAuthor={book.author}
-                //     bookIsbn={book.isbn}
-                // />
+                // <tr key={index}>
+                //     <td>{book.title}</td>
+                //     <td>{book.author}</td>
+                //     <td>{book.isbn}</td>
+                //     <td><a href="#" className="delete">X</a></td>
+                // </tr>
+                <BookItem 
+                    key={index}
+                    bookTitle={book.title}
+                    bookAuthor={book.author}
+                    bookIsbn={book.isbn}
+                />
             })
         }
     </tbody>
